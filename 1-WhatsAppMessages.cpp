7567967019c6c9cb538b5acd66fc01359edf73f3
alvarogonmar <alvarogonmar0@gmail.com>
2025-09-02 20:18:57 -0600

    Limit displayed chat messages to the first 10 in main function

--- 1-WhatsAppMessages.cpp
+++ 1-WhatsAppMessages.cpp
@@ -56,7 +56,7 @@ int main()
     vector<Message> chat = generateChatHistory(50);
 
     // Mostrar solo los primeros 10
-    for (int i = 0; i < 50; i++)
+    for (int i = 0; i < 10; i++)
     {
         cout << chat[i].id << " [" << chat[i].sender << "]: "
              << chat[i].text
